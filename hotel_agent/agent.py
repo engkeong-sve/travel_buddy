37ed89a9ad8b0d9d6c199c67e7b1c5a31222b389
Jocelyn <jocelynmg@hotmail.com>
2025-06-03 23:33:45 +0800

    added agent as a tool for hotel agent

--- hotel_agent/agent.py
+++ hotel_agent/agent.py
@@ -1,13 +1,14 @@
 from google.adk.agents import Agent
+from google.adk.tools.agent_tool import AgentTool
 from .sub_agents.hotel_booking_agent.agent import hotel_booking_agent
 from .sub_agents.hotel_cancellation_agent.agent import hotel_cancellation_agent
 from .sub_agents.hotel_search_agent.agent import hotel_search_agent
 
-root_agent = Agent(
-    name='root_agent',
+hotel_agent = Agent(
+    name='hotel_agent',
     model='gemini-2.0-flash',
     description='Hotel service agent',
-    instruction=f'''
+    instruction='''
     You are a hotel service agent.
     Your role is to help users with any query related to hotel, and direct them to respective specialized agent.
 
@@ -26,3 +27,23 @@ root_agent = Agent(
     ''',
     sub_agents=[hotel_search_agent, hotel_booking_agent, hotel_cancellation_agent],
 )
+
+
+joke_agent = Agent(
+    name='joke_agent',
+    model='gemini-2.0-flash',
+    description='Joke Agent',
+    instruction='You are a good joking agent that is able to generate puns and intellectual jokes to entertain human.'
+)
+
+
+root_agent = Agent(
+    name='root_agent',
+    model='gemini-2.0-flash',
+    description='Root Agent',
+    instruction='''
+    You are a root agent that is able to use different agent tools to perform the necessary functions required by user.
+    ''',
+    tools=[AgentTool(agent=hotel_agent), AgentTool(agent=joke_agent)]
+)
+
